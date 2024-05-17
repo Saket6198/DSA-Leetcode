6ae2f29021a4f79d0af8a2dddddef8ce8c9cb899
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-17 23:30:42 +0530

    directed

---
+++ graphs/directedAdjMat.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// adjacency matrix
+// undirected and unweighted graph
+
+int main(){
+    int vertex,edges;
+    printf("Enter no of vertex and edges: \n");
+    cin>>vertex>>edges;
+    vector<vector<bool> >AdjMat(vertex, vector<bool>(vertex, 0));
+
+
+    int u, v;
+     printf("Enter the edges present between vertices\n");
+    for(int i=0; i<edges; i++){ //tells which edges are present like 0-1, 0-3, 2-3 etc.
+       
+        cin>>u>>v;
+        AdjMat[u][v] = 1;
+    }
+    for(int i=0; i<vertex; i++){
+        for(int j=0; j<vertex; j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+}
