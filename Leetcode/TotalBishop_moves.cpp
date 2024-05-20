d919ec6949156db833c010497cf5965b891f7c89
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-20 13:05:19 +0530

    Total Bishop moves

---
+++ Leetcode/TotalBishop_moves.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Solution{
+public:
+    int BishopMoves(int A, int B){
+        int count = 0;
+        count += min(abs(A-8), abs(B-1));
+        count += min(abs(A-8), abs(B-8));
+        count += min(abs(A-1), abs(B-8));
+        count += min(abs(A-1), abs(B-1));
+    return count;
+
+    }
+};
+
+int main(){
+    Solution solution;
+    int a, b;
+    cout<<"enter the position of the bishop : ";
+    cin>>a>>b;
+    cout<<solution.BishopMoves(a, b);
+}
