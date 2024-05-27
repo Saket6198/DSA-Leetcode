10278147d533d589004157532ad237482f17e0ed
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-27 23:41:44 +0530

    Selection sort descending

---
+++ Selection Sort/select_sortdesc.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    void Selection_Sort(int arr[], int n){
+        int i, j;
+        for(i=0; i<n; i++){
+            int index = i;
+            for(j=i+1; j<n; j++){
+                if(arr[j] > arr[index])
+                    index = j;
+            }
+            swap(arr[i], arr[index]);
+        }
+    }
+};
+
+void Print_array(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
+int main(){
+    Solution sol;
+    int arr[10], n, i;
+    cout<<"Enter the size of the array :";
+    cin>>n;
+    cout<<"\nEnter the elements of the array : ";
+    for(i=0; i<n; i++)
+        cin>>arr[i];
+    sol.Selection_Sort(arr, n);
+    Print_array(arr, n);
+}
