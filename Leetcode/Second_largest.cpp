6c50cbc23d56faccd330bb5328a551873f5b25f4
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-27 10:17:19 +0530

    Second largest

---
+++ Leetcode/Second_largest.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<
+using namespace std;
+
+class Solution{
+public:
+    int SecondLargest(int arr[], int n){
+        for(int i = 0; i<sizeof(arr) / sizeof(int); i++)
+    }
+};
+
+int main(){
+    Solution solution;
+    int arr[100], x, size;
+    cout<<"Enter the size of array : ";
+    cin>>size;
+    cout<<"\nEnter the elements of the array : \n";
+    for(int i=0; i<size; i++)
+        cin>>arr[i];
+    cout<<solution.SecondLargest(arr, );
+}
