048f496034dde9d74d82068472c554a9ac6b8df9
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-28 00:45:01 +0530

    Decreasing Insertion Sorting

---
+++ Insertion Sort/insertion_sort_desc.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+// Worst time complexity = O(n^2)
+// Best time complexity = O(n)
+using namespace std;
+
+class Solution{
+public:
+    void Insertion_Sort(int arr[], int n){
+        int i, j;
+        for(i=1; i<n; i++){
+            for(j=i; j>0; j--){
+                if(arr[j] > arr[j-1])
+                    swap(arr[j], arr[j-1]);
+                else
+                    break;
+            }
+        }
+    }
+};
+
+void Print_array(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
+int main(){
+    Solution sol;
+    int arr[10], n, i;
+    cout<<"Enter the size of the array :";
+    cin>>n;
+    cout<<"\nEnter the elements of the array : ";
+    for(i=0; i<n; i++)
+        cin>>arr[i];
+    sol.Insertion_Sort(arr, n);
+    Print_array(arr, n);
+}
