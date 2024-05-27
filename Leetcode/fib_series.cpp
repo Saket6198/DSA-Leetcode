78f85279514252a4c3cc67fbaf7e165be1495b83
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-27 12:22:33 +0530

    Fib sereis

---
+++ Leetcode/fib_series.cpp
@@ -0,0 +1,75 @@
+/*
+509. Fibonacci Number
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+Constraints:
+
+0 <= n <= 30
+*/
+
+//  RECURSIVE approach 
+
+#include<iostream>
+
+using namespace std;
+/*
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        return fib(n-1) + fib(n-2);   
+    }
+};
+*/
+// ITERATIVE APPROACH(FASTER)
+class Solution {
+public:
+    int fib(int n) {
+        int sum, f=0, s=1;
+        if(n==0)
+            return 0;
+        if(n <= 2)
+            return 1;
+        for(int i=2; i<=n; i++){
+            sum = f +s;
+            f=s;
+            s=sum;
+        }
+        return sum;
+    }
+};
+
+int main(){
+    Solution sol;
+    int n;
+    cout<<"enter no:";
+    cin>>n;
+    cout<<sol.fib(n);
+}
