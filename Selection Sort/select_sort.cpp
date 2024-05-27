49233337fd02378502076c0d28d522ca6b1fcca0
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-27 21:56:43 +0530

    Selection-Sort Algo

--- Selection Sort/select_sort.cpp
+++ Selection Sort/select_sort.cpp
@@ -10,24 +10,25 @@ public:
             int index = i;
             for(j=i+1; j<n; j++){
                 if(arr[j] < arr[index])
-                    arr[index] = arr[j];
+                    index = j;
             }
+            swap(arr[i], arr[index]);
         }
     }
 };
 
-int Print_array(int arr[], int n){
+void Print_array(int arr[], int n){
     for(int i=0; i<n; i++)
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
 }
 int main(){
     Solution sol;
-    int arr[10], n, i, j;
+    int arr[10], n, i;
     cout<<"Enter the size of the array :";
     cin>>n;
     cout<<"\nEnter the elements of the array : ";
     for(i=0; i<n; i++)
         cin>>arr[i];
     sol.Selection_Sort(arr, n);
-    cout<<Print_array;
+    Print_array(arr, n);
 }
