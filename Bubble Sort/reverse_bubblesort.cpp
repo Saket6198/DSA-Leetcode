c8b708531ee122efc3f16a3badd18fcaa06e4670
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-27 23:59:32 +0530

    Reversed Bubble-Sort

--- Bubble Sort/reverse_bubblesort.cpp
+++ Bubble Sort/reverse_bubblesort.cpp
@@ -8,8 +8,8 @@ public:
     void Bubble_Sort(int arr[], int n){
         int i, j;
         bool exit=0;
-        for(i=n-1; i>=0; i--){
-            for(j=0; j<i; j++){
+        for(i=0; i<n-1; i++){
+            for(j=n-1; j>=i; j--){
                 if(arr[j] > arr[j+1]){
                     exit = 1;
                     swap(arr[j], arr[j+1]);
