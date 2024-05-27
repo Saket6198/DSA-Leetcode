a8997a9dffcf80444c3e1cb2f21360dcda8ba967
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-05-27 23:57:19 +0530

    Decreasing sort in bubble-sorting

---
+++ Bubble Sort/bubble_sort_desc.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+// Worst time complexity = O(n^2)
+// Best time complexity = O(n)
+using namespace std;
+
+class Solution{
+public:
+    void Bubble_Sort(int arr[], int n){
+        int i, j;
+        bool exit=0;
+        for(i=n-1; i>=0; i--){
+            for(j=0; j<i; j++){
+                if(arr[j] < arr[j+1]){
+                    exit = 1;
+                    swap(arr[j], arr[j+1]);
+                }
+            }
+            if(!exit)
+                return;
+        }
+    }
+};
+
+void Print_array(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
+int main(){
+    Solution sol;
+    int arr[10], n, i;
+    cout<<"Enter the size of the array :";
+    cin>>n;
+    cout<<"\nEnter the elements of the array : ";
+    for(i=0; i<n; i++)
+        cin>>arr[i];
+    sol.Bubble_Sort(arr, n);
+    Print_array(arr, n);
+}
