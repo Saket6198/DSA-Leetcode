41c96db78ece81818b0d3bb9c9e8aec134326079
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-02 22:50:29 +0530

    Reversing String using STL

---
+++ Leetcode/Strings/Reverse_string.cpp
@@ -0,0 +1,49 @@
+/*
+344. Reverse String
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(), s.end());
+    }
+};
+
+int main(){
+    Solution sol;
+    int size;
+    cout<<"Enter the size of the string : ";
+    cin>>size;
+    vector<char>arr;
+    cout<<"Enter the characters : ";
+    for(auto i=0; i<arr.size(); i++){
+        cin>>arr[i];
+    }
+    sol.reverseString(arr);
