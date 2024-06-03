498af056814ecf574b52504a0f7428f3a33a0185
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-03 17:26:13 +0530

    Append Characters to String to Make Subsequence

---
+++ Leetcode/Strings/Append_char.cpp
@@ -0,0 +1,66 @@
+/*
+2486. Append Characters to String to Make Subsequence
+
+You are given two strings s and t consisting of only lowercase English letters.
+
+Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+ 
+
+Example 1:
+
+Input: s = "coaching", t = "coding"
+Output: 4
+Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+Now, t is a subsequence of s ("coachingding").
+It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+Example 2:
+
+Input: s = "abcde", t = "a"
+Output: 0
+Explanation: t is already a subsequence of s ("abcde").
+Example 3:
+
+Input: s = "z", t = "abcde"
+Output: 5
+Explanation: Append the characters "abcde" to the end of s so that s = "zabcde".
+Now, t is a subsequence of s ("zabcde").
+It can be shown that appending any 4 characters to the end of s will never make t a subsequence.
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 105
+s and t consist only of lowercase English letters.
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    int appendCharacters(string s, string t){
+        auto first = 0, second =0;  
+        while(first <s.size() && second<t.size()){
+            if(s[first] == t[second]){
+                second++;
+            }
+            else if(s[first] != t[second]){
+            }
+            first++;
+        }
+        return t.size() - second;
+    }
+};
+int main(){
+    Solution sol;
+    string s, t;
+    cout<<"enter the string S: ";
+    cin>>s;
+    cout<<"enter the string t: ";
+    cin>>t;
+    cout<<sol.appendCharacters(s,t);
+}
