55c6574119553b86873856ee7d112d00083c905e
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-06 13:03:47 +0530

    Creating a suffix sum array from end

--- Leetcode/Kadane's Algorithm/simple_suffix_sum.cpp
+++ Leetcode/Kadane's Algorithm/simple_suffix_sum.cpp
@@ -7,16 +7,16 @@ int main(){
     cout<<"Enter the size of the array : ";
     cin>>size;
     cout<<"enter the elements of the array : ";
-    vector<int>arr(size);
-    for(int i=0; i<arr.size(); i++){
+    int arr[size];
+    for(int i=0; i<size; i++){
         cin>>arr[i];
     }
     int ans[size];
-    ans[-1] = arr[arr.size() - 1];
-    for(int i=arr.size()-2 ;i>=0; i--){
-        ans[i] = ans[arr[i] + ans[i+1]];
+    ans[size-1] = arr[size - 1];
+    for(int i=size-2 ;i>=0; i--){
+        ans[i] = arr[i] + ans[i+1];
     }
-    for(auto i=0; i<arr.size()-1; i++){
-        cout<<arr[i]<<" ";
+    for(auto i=0; i<size; i++){
+        cout<<ans[i]<<" ";
     }
 }
