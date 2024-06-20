004852d2c002c6fe831c4b4af4cd5562d81fd837
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-20 12:21:30 +0530

    Swapping nodes using count

---
+++ Leetcode/LL/swapnodes_pair.cpp
@@ -0,0 +1,62 @@
+/*
+24. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+*/
+
+#include<iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head -> next == NULL)
+            return head;
+        ListNode *it = head;
+        ListNode *prev = NULL;
+        int c = 1;
+        while(it){
+            if(c==2){
+                c=1;
+                swap(prev->val, it->val);
+            }else{
+                c++;
+            }
+            prev = it;
+            it = it -> next;
+        }
+        return head;
+    }
+};
