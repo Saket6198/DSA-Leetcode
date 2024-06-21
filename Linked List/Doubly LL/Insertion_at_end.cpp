196405657a27ab7743a7e49359bfff20aadc5d27
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-21 19:17:56 +0530

    Inserting nodes at the start of Doubly Linked List

---
+++ Linked List/Doubly LL/Insertion_at_end.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int value){
+        data = value;
+        next = prev = NULL;
+    }
+};
+
+int main(){
+    Node *head = NULL, *tail = NULL;
+    vector<int>arr = {1, 2, 3, 4};
+    for(auto i : arr){
+        if(head == NULL){
+            head = new Node(i);
+            tail = head;
+        }else{
+            Node *temp = new Node(i);
+            tail -> next = temp;
+            temp -> prev = tail;
+            tail = temp;
+        }
+    }
+    Node *it = head;
+    while(it){
+        cout<<it->data<<"->";
+        it = it -> next;
+    }
+}
