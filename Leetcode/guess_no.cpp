3feede8f1fdb191b0e32358d5df49de8b18bc327
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-22 12:52:19 +0530

    guessing the correct no using BS

---
+++ Leetcode/guess_no.cpp
@@ -0,0 +1,72 @@
+/*
+
+374. Guess Number Higher or Lower
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+ 
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+*/
+
+#include<iostream>
+
+using namespace std;
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        if(n == 1){
+            return 1;
+        }
+        int s = 1, e = n, m;
+        while(s <= e){
+            m = s + (e-s)/2;
+            int a = guess(m);
+            if(a == 0)
+                return m;
+            else if(a == -1)
+                e = m - 1;
+            else
+                s = m + 1;
+        }
+    return s;
+    }
+};
