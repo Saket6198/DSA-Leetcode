c07d3726a48d16f7ee7678f41222f5e7b5a9f09b
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-24 13:25:47 +0530

    finding intersection using common difference b/w 2 nodes and Floyd Cycle detection

--- Leetcode/LL/intersection_of_2Y_LL.cpp
+++ Leetcode/LL/intersection_of_2Y_LL.cpp
@@ -109,14 +109,6 @@ public:
 };
 /*  using floyd cycle detection */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
