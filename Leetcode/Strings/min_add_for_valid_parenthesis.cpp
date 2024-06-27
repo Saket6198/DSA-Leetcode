4cc33989337ad79471794cb69c0e0b5e4da4cbc3
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-06-27 14:10:25 +0530

    returning min no of parenthesis to add to make it a valid using stack and with two pointer

---
+++ Leetcode/Strings/min_add_for_valid_parenthesis.cpp
@@ -0,0 +1,90 @@
+/*
+
+921. Minimum Add to Make Parentheses Valid
+
+A parentheses string is valid if and only if:
+
+It is the empty string,
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+
+For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+
+ 
+
+Example 1:
+
+Input: s = "())"
+Output: 1
+Example 2:
+
+Input: s = "((("
+Output: 3
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] is either '(' or ')'.
+*/
+
+#include<iostream>
+#include<stack>
+
+using namespace std;
+/* using stack SC: O(N)*/
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        if(s.size() == 1)
+            return 1;
+        stack<char>st;
+        int i =0;
+        while(i < s.size()){
+            if(s[i] =='('){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    st.push(s[i]);
+                }
+                else if((st.top() == '(' && s[i] == ')'))
+                    st.pop();
+                else if(st.top() == ')')
+                    st.push(s[i]);
+            }
+            i++;
+        }
+        return st.size();
+    }
+};
+
+/* Without stack SC: O(1) */
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0, close = 0;
+        for(auto i=0; i<s.size(); i++){
+            if(s[i] == '(')
+                open++;
+            else
+                if(open == 0)
+                    close++;
+                else if(open != 0 && s[i] == ')'){
+                open--;
+                }else
+                    close++;
+        }
+        return (open + close);
+    }
+};
+
+int main(){
+    Solution sol;
+    string s;
+    cout<<"enter the string () : ";
+    cin>>s;
+    cout<<"Min no of steps needed to make a valid parenthesis : "<<sol.minAddToMakeValid(s);
+}
