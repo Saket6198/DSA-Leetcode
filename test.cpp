fc1999998e9aa6d9e35cdfd37361abf3867d304e
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-09 22:03:42 +0530

    test file

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+public:
+    int Quad(vector<int>& arr, int S){
+        vector<int>ans(4);
+        for(int i = 0; i<arr.size() - 2; i++){
+            for(int j = i + 1; j<arr.size() - 1; j++){
+                for(int k = j + 1; k<arr.size(); k++){
+                    
+                }
+            }
+        }
+    }
+};
