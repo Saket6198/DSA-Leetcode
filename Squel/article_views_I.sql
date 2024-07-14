12a7428ea6f284f306a4ce7dcf7d7545455baf22
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-14 21:00:02 +0530

    find all the authors that viewed at least one of their own articles(distinct, Order By)

---
+++ Squel/article_views_I.sql
@@ -0,0 +1,70 @@
+/*
+1148. Article Views I
+
+SQL Schema
+Create table If Not Exists Views (article_id int, author_id int, viewer_id int, view_date date)
+Truncate table Views
+insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '5', '2019-08-01')
+insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '6', '2019-08-02')
+insert into Views (article_id, author_id, viewer_id, view_date) values ('2', '7', '7', '2019-08-01')
+insert into Views (article_id, author_id, viewer_id, view_date) values ('2', '7', '6', '2019-08-02')
+insert into Views (article_id, author_id, viewer_id, view_date) values ('4', '7', '1', '2019-07-22')
+insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4', '4', '2019-07-21')
+insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4', '4', '2019-07-21')
+
+Pandas Schema
+data = [[1, 3, 5, '2019-08-01'], [1, 3, 6, '2019-08-02'], [2, 7, 7, '2019-08-01'], [2, 7, 6, '2019-08-02'], [4, 7, 1, '2019-07-22'], [3, 4, 4, '2019-07-21'], [3, 4, 4, '2019-07-21']]
+views = pd.DataFrame(data, columns=['article_id', 'author_id', 'viewer_id', 'view_date']).astype({'article_id':'Int64', 'author_id':'Int64', 'viewer_id':'Int64', 'view_date':'datetime64[ns]'})
+
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+Output: 
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+
+
+*/
+
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+ORDER BY author_id ASC;
