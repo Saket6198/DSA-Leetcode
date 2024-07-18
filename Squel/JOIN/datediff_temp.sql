87128d4ee47f1ac9ee6cec921c64faf3c5323252
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-18 13:56:01 +0530

    finding records in date using Subdate and datediff function,self join

---
+++ Squel/JOIN/datediff_temp.sql
@@ -0,0 +1,81 @@
+/*
+197. Rising Temperature
+SQL Schema
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+
+Pandas Schema
+data = [[1, '2015-01-01', 10], [2, '2015-01-02', 25], [3, '2015-01-03', 20], [4, '2015-01-04', 30]]
+weather = pd.DataFrame(data, columns=['id', 'recordDate', 'temperature']).astype({'id':'Int64', 'recordDate':'datetime64[ns]', 'temperature':'Int64'})
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+*/
+
+/* Using Join */
+
+select t1.id 
+from Weather t1
+join Weather t2
+on datediff(t1.recordDate, t2.recordDate) = 1 and t1.temperature > t2.temperature;
+
+/* Without Join */
+
+select t1.id 
+from Weather t1, Weather t2
+where datediff(t1.recordDate, t2.recordDate) = 1 and t1.temperature > t2.temperature;
+
+/* Using SubDate Function */
+
+select t1.id 
+from Weather t1, Weather t2
+where subdate(t1.recordDate, 1) = t2.recordDate and t1.temperature > t2.temperature;
+
