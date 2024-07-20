76cef1922cfd2223d26f71175bb6a71eae021a10
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-20 12:13:41 +0530

    finding employee bonus using left join in sql and panda

---
+++ Squel/JOIN/employee_bonus.sql
@@ -0,0 +1,105 @@
+/*
+577. Employee Bonus
+
+SQL Schema
+Create table If Not Exists Employee(empId int, name varchar(25), supervisor int, salary int)
+Create table If Not Exists Bonus (empId int, bonus int)
+Truncate table Employee
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', 'None', '4000')
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000')
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000')
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000')
+Truncate table Bonus
+insert into Bonus (empId, bonus) values ('2', '500')
+insert into Bonus (empId, bonus) values ('4', '2000')
+
+Pandas Schema
+data = [[3, 'Brad', None, 4000], [1, 'John', 3, 1000], [2, 'Dan', 3, 2000], [4, 'Thomas', 3, 4000]]
+employee = pd.DataFrame(data, columns = ['empId', 'name', supervisor, salary]).astype({'empId': 'Int64', 'name': 'object', 'supervisor': 'Int64', 'salary': 'Int64'})
+data = [[2, 500], [4, 2000]]
+Bonus = pd.DataFrame(data, columns = ['empId', 'bonus']).astype({'empId': 'Int64', 'bonus': 'Int64'})
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+
+Table: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+ 
+
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
+
+
+*/
+
+
+select e1.name, e2.bonus
+from Employee e1
+left join Bonus e2
+using(empId)
+where e2.bonus < 1000 or e2.bonus is null; 
+
+/* Using pandas */
+
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    merged_df = employee.merge(bonus, on = 'empId', how = 'left')
+    filtered_df = merged_df[(merged_df['bonus'] < 1000) | merged_df['bonus'].isnull()]
+    employee_bonus = filtered_df[['name', 'bonus']]
+    
+    return employee_bonus
+
