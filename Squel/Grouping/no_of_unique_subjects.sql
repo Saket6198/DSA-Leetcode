03b2a5d74cf3e7899e764fac15367e9d433dcefd
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-23 23:43:36 +0530

    finding no of unique sub taught by each teacher using sort and grouping

---
+++ Squel/Grouping/no_of_unique_subjects.sql
@@ -0,0 +1,82 @@
+/*
+2356. Number of Unique Subjects Taught by Each Teacher
+
+SQL Schema
+Create table If Not Exists Teacher (teacher_id int, subject_id int, dept_id int)
+Truncate table Teacher
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '3')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '4')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '3', '3')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '1', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '2', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '3', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '4', '1')
+
+Pandas Schema
+data = [[1, 2, 3], [1, 2, 4], [1, 3, 3], [2, 1, 1], [2, 2, 1], [2, 3, 1], [2, 4, 1]]
+teacher = pd.DataFrame(data, columns=['teacher_id', 'subject_id', 'dept_id']).astype({'teacher_id':'Int64', 'subject_id':'Int64', 'dept_id':'Int64'})
+
+Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ 
+
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+
+The result format is shown in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+Explanation: 
+Teacher 1:
+  - They teach subject 2 in departments 3 and 4.
+  - They teach subject 3 in department 3.
+Teacher 2:
+  - They teach subject 1 in department 1.
+  - They teach subject 2 in department 1.
+  - They teach subject 3 in department 1.
+  - They teach subject 4 in department 1.
+
+*/
+select teacher_id, count(distinct subject_id) as cnt
+from Teacher
+group by teacher_id;
+
+/* Using Pandas */
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    final_df = teacher.groupby(by='teacher_id', as_index=False).agg(cnt=('subject_id','nunique'))
+    return final_df
