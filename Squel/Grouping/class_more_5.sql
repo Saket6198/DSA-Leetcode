1add897102c09093bf2eb8c9e4b3079895bb0951
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-27 12:31:52 +0530

    finding class count by student from groupby and having clause

---
+++ Squel/Grouping/class_more_5.sql
@@ -0,0 +1,74 @@
+/*
+596. Classes More Than 5 Students
+
+SQL Schema
+Create table If Not Exists Courses (student varchar(255), class varchar(255))
+Truncate table Courses
+insert into Courses (student, class) values ('A', 'Math')
+insert into Courses (student, class) values ('B', 'English')
+insert into Courses (student, class) values ('C', 'Math')
+insert into Courses (student, class) values ('D', 'Biology')
+insert into Courses (student, class) values ('E', 'Math')
+insert into Courses (student, class) values ('F', 'Computer')
+insert into Courses (student, class) values ('G', 'Math')
+insert into Courses (student, class) values ('H', 'Math')
+insert into Courses (student, class) values ('I', 'Math')
+
+Pandas Schema
+data = [['A', 'Math'], ['B', 'English'], ['C', 'Math'], ['D', 'Biology'], ['E', 'Math'], ['F', 'Computer'], ['G', 'Math'], ['H', 'Math'], ['I', 'Math']]
+courses = pd.DataFrame(data, columns=['student', 'class']).astype({'student':'object', 'class':'object'})
+
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+
+*/
+select class
+from Courses
+group by class
+having count(class) >= 5;
