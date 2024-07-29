736cf3f12b79299d7d09d814d3d8b58eb1bbf1a3
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-30 00:01:20 +0530

    all queue stl library functions

--- Queue/queue_stl.cpp
+++ Queue/queue_stl.cpp
@@ -8,11 +8,11 @@ int main(){
     queue<int>q;
     q.push(5);
     q.push(10);
-    q.pop();
-    q.push(8);
+    // q.pop();
+    // q.push(8);
 
     cout<<q.front()<<endl;
     int b = q.back();
     cout<<b;
-    cout<<q.size();
+    cout<<endl<<"size: "<<q.size();
 }
