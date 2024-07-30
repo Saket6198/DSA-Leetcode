8620139a0423d69615b5a513ffc6bf7c13fce0c9
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-30 23:10:24 +0530

    using recursive addition and recursive counter stack to calc total nodes in a BT

---
+++ Leetcode/Binary Tree/count_complete_nodes.cpp
@@ -0,0 +1,84 @@
+/*
+222. Count Complete Tree Nodes
+Solved
+Easy
+Topics
+Companies
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+/* Using Integer recursion  */
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);    // counts the total no of nodes on left and right, else return NULL
+    }
+};
+
+/* Using a counter recursive */
+
+class Solution {
+public:
+
+    int countNodes(TreeNode* root) {
+        int count = 0;
+        total(root, count);
+        return count;
+    }
+
+    void total(TreeNode* root, int &count){     // pass by reference
+        if (root == NULL)
+            return;
+        
+        count++;
+        total(root->left, count);   // increases count whenever root -> left is not null
+        total(root->right, count);
+    }
+};
