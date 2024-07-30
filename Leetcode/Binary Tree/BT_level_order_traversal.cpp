17f6ddab08e95b63844dad868cd3ddc819684bea
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-07-30 20:50:11 +0530

    traversing a BT level wise using 2d matrix and TreeNode* queue

--- Leetcode/Binary Tree/BT_level_order_traversal.cpp
+++ Leetcode/Binary Tree/BT_level_order_traversal.cpp
@@ -71,3 +71,33 @@ public:
     }
 };
 
+/* Using Count Method */
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root)
+            return {};
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int s = q.size();
+            vector<int>row;
+            while(s > 0){
+                TreeNode* temp = q.front();
+                row.push_back(temp->val);
+                q.pop();
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+                s--;
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
