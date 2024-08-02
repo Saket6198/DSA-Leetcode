86a83fad5a94646141f81eee9919745814177bc7
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-08-02 22:07:05 +0530

    changing dtype using .astype() function

---
+++ pandas/change_data_type.py
@@ -0,0 +1,52 @@
+'''
+
+2886. Change Data Type
+Solved
+Easy
+Companies
+Hint
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
+| grade       | float  |
++-------------+--------+
+Write a solution to correct the errors:
+
+The grade column is stored as floats, convert it to integers.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+Input:
+DataFrame students:
++------------+------+-----+-------+
+| student_id | name | age | grade |
++------------+------+-----+-------+
+| 1          | Ava  | 6   | 73.0  |
+| 2          | Kate | 15  | 87.0  |
++------------+------+-----+-------+
+Output:
++------------+------+-----+-------+
+| student_id | name | age | grade |
++------------+------+-----+-------+
+| 1          | Ava  | 6   | 73    |
+| 2          | Kate | 15  | 87    |
++------------+------+-----+-------+
+
+'''
+
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade'] = students['grade'].astype(int)
+    return students
+
+    # #or 
+    students = students.astype({'grade': int})
+    return students
