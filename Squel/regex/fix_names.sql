d24ce526b9100a849cba56069f389cce883b664c
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-08-08 23:16:32 +0530

    fixing usernames using concat, upper and substr functions to capitalize

---
+++ Squel/regex/fix_names.sql
@@ -0,0 +1,56 @@
+/*
+1667. Fix Names in a Table
+
+SQL Schema
+Create table If Not Exists Users (user_id int, name varchar(40))
+Truncate table Users
+insert into Users (user_id, name) values ('1', 'aLice')
+insert into Users (user_id, name) values ('2', 'bOB')
+
+Pandas Schema
+data = [[1, 'aLice'], [2, 'bOB']]
+users = pd.DataFrame(data, columns=['user_id', 'name']).astype({'user_id':'Int64', 'name':'object'})
+
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+*/
+# Write your MySQL query statement below
+select user_id, concat(upper(substr(name,1,1)), lower(substr(name, 2))) as name
+from Users
+order by user_id;
