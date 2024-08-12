6c20ccb9b486f99745d725ca8c58556b555e6ea0
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-08-12 22:20:58 +0530

    finding symmetricity b/w left and right single tree using identical and mirroring recursive fn

--- Leetcode/Binary Tree/SymmetricTrees.cpp
+++ Leetcode/Binary Tree/SymmetricTrees.cpp
@@ -4,7 +4,7 @@
 
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
- 
+
 
 Example 1:
 
@@ -16,58 +16,65 @@ Example 2:
 
 Input: root = [1,2,2,null,3,null,3]
 Output: false
- 
+
 
 Constraints:
 
 The number of nodes in the tree is in the range [1, 1000].
 -100 <= Node.val <= 100
- 
+
 
 Follow up: Could you solve it both recursively and iteratively?
 */
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 // Definition for a binary tree node.
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}   
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* mirror(TreeNode* root){
-        if(!root)
+    TreeNode *mirror(TreeNode *root)
+    {
+        if (!root)
             return NULL;
         TreeNode *temp = root->left;
-        root -> left = root -> right;
-        root -> right = temp;
-        mirror(root -> left);
-        mirror(root -> right);
+        root->left = root->right;
+        root->right = temp;
+        mirror(root->left);
+        mirror(root->right);
         return root;
     }
-    bool isIdentical(TreeNode* r1, TreeNode* r2){
-        if(!r1 && !r2)
+    bool isIdentical(TreeNode *r1, TreeNode *r2)
+    {
+        if (!r1 && !r2)
             return 1;
-        if((!r1 && r2)|| (r1 && !r2)){
+        if ((!r1 && r2) || (r1 && !r2))
+        {
             return 0;
         }
-        if(r1->val != r2 ->val)
+        if (r1->val != r2->val)
             return 0;
-        return((isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right)));
+        return ((isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right)));
     }
-    bool isSymmetric(TreeNode* root) {
-        if(!root){
+    bool isSymmetric(TreeNode *root)
+    {
+        if (!root)
+        {
             return 1;
         }
-        TreeNode* lnode = mirror(root->left);
+        TreeNode *lnode = mirror(root->left);
         bool result = isIdentical(lnode, root->right);
         return result;
     }
