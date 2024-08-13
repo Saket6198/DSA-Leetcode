ed270014fc4fd52eaf645dcdab506c225be64ccb
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-08-13 22:11:12 +0530

    traversing in a spiral format using BFS and reverse adding using deque

--- Leetcode/Binary Tree/zigzag_BT_level_traversal.cpp
+++ Leetcode/Binary Tree/zigzag_BT_level_traversal.cpp
@@ -44,17 +44,6 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
