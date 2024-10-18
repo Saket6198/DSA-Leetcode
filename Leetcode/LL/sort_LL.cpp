8d666f8974cd3d1c983d92280b4dcc6b2e927848
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-10-18 20:52:42 +0530

    sorting a LL using vector

---
+++ Leetcode/LL/sort_LL.cpp
@@ -0,0 +1,74 @@
+/*
+148. Sort List
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode *i = head;
+        vector<int>arr;
+        while(i){
+            arr.push_back(i->val);
+            i = i -> next;
+        }
+        i = head;
+        sort(arr.begin(), arr.end());
+        for(int j = 0; j<arr.size(); j++){
+            i -> val = arr[j]; 
+            i = i -> next;
+        }
+        return head;
+    }
+};
+
+
+// constant space ?
