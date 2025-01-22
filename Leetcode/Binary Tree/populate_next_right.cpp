73e89d63809bcf80acb6b70116111e339a256608
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-01-22 21:37:18 +0530

    style: format code and improve readability in populate_next_right.cpp

--- Leetcode/Binary Tree/populate_next_right.cpp
+++ Leetcode/Binary Tree/populate_next_right.cpp
@@ -14,7 +14,7 @@ Populate each next pointer to point to its next right node. If there is no next 
 
 Initially, all next pointers are set to NULL.
 
- 
+
 
 Example 1:
 
@@ -26,13 +26,13 @@ Example 2:
 
 Input: root = []
 Output: []
- 
+
 
 Constraints:
 
 The number of nodes in the tree is in the range [0, 212 - 1].
 -1000 <= Node.val <= 1000
- 
+
 
 Follow-up:
 
@@ -41,41 +41,47 @@ The recursive approach is fine. You may assume implicit stack space does not cou
 
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int val;
-    Node* left;
-    Node* right;
-    Node* next;
+    Node *left;
+    Node *right;
+    Node *next;
 
     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
 
     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
 
-    Node(int _val, Node* _left, Node* _right, Node* _next)
+    Node(int _val, Node *_left, Node *_right, Node *_next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 
-
-class Solution {
+class Solution
+{
 public:
-    Node* connect(Node* root) {
-        if(!root) return nullptr;
-        queue<Node*> q;
-        q.push(root);        
-        while(q.size()) {
-            Node* rightNode = nullptr;                    // set rightNode to null initially
-            for(int i = size(q); i; i--) {                // traversing each level
-                auto cur = q.front(); q.pop();            // pop a node from current level and,
-                cur -> next = rightNode;                  // set its next pointer to rightNode
-                rightNode = cur;                          // update rightNode as cur for next iteration
-                if(cur -> right)                          // if a child exists
-                    q.push(cur -> right),                 // IMP: push right first to do right-to-left BFS
-                    q.push(cur -> left);                  // then push left
+    Node *connect(Node *root)
+    {
+        if (!root)
+            return nullptr;
+        queue<Node *> q;
+        q.push(root);
+        while (q.size())
+        {
+            Node *rightNode = nullptr; // set rightNode to null initially
+            for (int i = q.size(); i; i--)
+            { // traversing each level
+                auto cur = q.front();
+                q.pop();                   // pop a node from current level and,
+                cur->next = rightNode;     // set its next pointer to rightNode
+                rightNode = cur;           // update rightNode as cur for next iteration
+                if (cur->right)            // if a child exists
+                    q.push(cur->right),    // IMP: push right first to do right-to-left BFS
+                        q.push(cur->left); // then push left
             }
         }
         return root;
