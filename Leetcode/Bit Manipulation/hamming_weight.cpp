93d098d46e15604c5dc4a45a65b73c027e890e07
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-03-07 23:00:11 +0530

    Implement solution for Hamming Weight problem using bit manipulation.

---
+++ Leetcode/Bit Manipulation/hamming_weight.cpp
@@ -0,0 +1,64 @@
+/*
+
+191. Number of 1 Bits
+
+Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+ 
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.
+
+Example 2:
+
+Input: n = 128
+
+Output: 1
+
+Explanation:
+
+The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+
+Input: n = 2147483645
+
+Output: 30
+
+Explanation:
+
+The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+ 
+
+Follow up: If this function is called many times, how would you optimize it?
+*/
+
+#include <bits/stdc++.h>
+using namespace std;    
+
+class Solution {
+    public:
+        int hammingWeight(int n) {
+            int cnt = 0;
+            while(n != 0){
+                if(n & 1)
+                    cnt++;
+                n >>= 1;
+            }
+            return cnt;
+        }
+    };
+
