8d58736a5e0869de114bf3394350cf337f2145f0
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-05-13 01:46:17 +0530

    best optimized solution using prechecks

--- Leetcode/finding_3_digit_no.cpp
+++ Leetcode/finding_3_digit_no.cpp
@@ -97,6 +97,34 @@ class Solution {
         }
     };
 
+    /* best optimized in TC: O(450) by checking beforehand*/
+
+class Solution {
+    public:
+        vector<int> findEvenNumbers(vector<int>& digits) {
+            int freq[10] = {};
+            vector<int>ans;
+            for(auto i : digits)
+                freq[i]++;
+            for(int i=1; i<=9; i++){
+                if(freq[i] == 0) continue;
+                freq[i]--;
+                for(int j=0; j<=9; j++){
+                    if(freq[j] == 0) continue;
+                    freq[j]--;
+                    for(int k=0; k<=8; k+= 2){
+                        if(freq[k] == 0) continue;
+                        freq[k]--;
+                    ans.push_back(i*100 + j*10 + k);
+                    freq[k]++;
+                    }
+                    freq[j]++;
+                }
+                freq[i]++;
+            }
+            return ans;
+        }
+    };
 
 
 
