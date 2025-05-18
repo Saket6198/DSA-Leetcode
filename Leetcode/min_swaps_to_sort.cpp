5a805a27b886f2c86bd978b6a49ad357b2ebef98
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-05-18 14:54:18 +0530

    updated hashmap method with comments for better understanding

--- Leetcode/min_swaps_to_sort.cpp
+++ Leetcode/min_swaps_to_sort.cpp
@@ -23,27 +23,40 @@ Constraints:
 using namespace std;
 
 
-class Solution {
-  public:
-    // Function to find the minimum number of swaps required to sort the array.
-    int minSwaps(vector<int>& arr) {
-        vector<int>temp(arr.begin(), arr.end());
-        sort(temp.begin(), temp.end());
-        unordered_map<int, int>seen;
-        for(int i=0; i<arr.size(); i++){
-            seen[arr[i]] = i;
+#include <bits/stdc++.h>
+using namespace std;
+
+int minSwaps(vector<int> &arr) {
+    
+    // Temporary array to store elements in sorted order
+    vector<int> temp(arr.begin(), arr.end());
+    sort(temp.begin(), temp.end());
+    
+  	// Hashing elements with their correct positions
+  	unordered_map<int, int> pos; 
+    for(int i = 0; i < arr.size(); i++)
+        pos[arr[i]] = i;
+    
+  	int swaps = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        if(temp[i] != arr[i]) {
+            
+            // Index of the element that should be at index i.
+            int ind = pos[temp[i]];
+            swap(arr[i], arr[ind]);
+            
+            // Update the indices in the hashmap
+            pos[arr[i]] = i; 
+            pos[arr[ind]] = ind;
+            
+            swaps++; 
         }
-        int cnt = 0;
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i] != temp[i]){
-                int idx = seen[temp[i]];
-                swap(arr[i], arr[idx]);
-                cnt++;
-                
-                seen[arr[i]] = i;
-                seen[arr[idx]] = idx;
-            }
-        }
-        return cnt;
     }
-};
+    return swaps; 
+}
+
+int main() {
+    vector<int> arr = {10, 19, 6, 3, 5}; 
+    cout << minSwaps(arr); 
+    return 0;
+}
