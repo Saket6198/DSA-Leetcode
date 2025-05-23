5625de12d6c382a5a48c6b84405acb6b8515e801
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-05-23 21:02:22 +0530

    two sum using iterative approach BST

--- Leetcode/BST/IV_two_sum_BST.cpp
+++ Leetcode/BST/IV_two_sum_BST.cpp
@@ -54,3 +54,36 @@ public:
         return findpair(root, seen, k);
     }
 };
+
+/* Iterative Approach */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        stack<TreeNode*>st;
+        st.push(root);
+        unordered_set<int>seen;
+        while(!st.empty()){
+            auto temp = st.top();
+            st.pop();
+            if(seen.count((k- temp->val)))
+                return 1;
+            seen.insert(temp->val);
+            if(temp->right)
+                st.push(temp->right);
+            if(temp->left)
+                st.push(temp->left);
+        }
+        return 0;
+    }
+};
