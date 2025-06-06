5cb27c3c31c02dfbb593b05a4fdae8c87a7f8573
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-06-06 15:20:34 +0530

    finding all root to leaf path sum equal to target using dynamic stack tuple method

---
+++ Leetcode/Binary Tree/II_path_sum.cpp
@@ -0,0 +1,86 @@
+/*
+
+113. Path Sum II
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool traversal(TreeNode* root, int sum, int &target, vector<int>temp, vector<vector<int>>&ans){
+        if(!root)
+            return 0;
+        
+        sum += root->val;
+        temp.push_back(root->val);
+        if(!root->left && !root->right)
+            if(target == sum){
+                ans.push_back(temp);
+            }
+        return (traversal(root->left, sum, target, temp, ans) ||
+         traversal(root->right, sum, target, temp, ans));
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int>temp;
+        vector<vector<int>>ans;
+        traversal(root, 0, targetSum, temp, ans);
+        return ans;
+    }
+};
+
