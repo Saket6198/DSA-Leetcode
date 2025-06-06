6a3937dab132effac80079617113699114e0404d
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-06-06 15:18:18 +0530

    finding total number of root to leaf path sum using temp stack tuple method

---
+++ Leetcode/Binary Tree/sum_root_leave.cpp
@@ -0,0 +1,82 @@
+/*129. Sum Root to Leaf Numbers
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        stack<tuple<TreeNode*, int>>st;
+        st.push({root, root->val});
+        int sum = 0;
+        while(!st.empty()){
+            auto [temp, path] = st.top();
+            st.pop();
+            if(!temp->left && !temp->right){
+                sum += path;
+            }
+            if(temp->right){
+                st.push({temp->right, path*10+temp->right->val});
+            }
+            
+            if(temp->left){
+                st.push({temp->left, path*10+temp->left->val});
+            }
+        }
+        return sum;
+    }
+};
